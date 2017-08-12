3060a7b00e9cf9d012fbafc6f0956ef89ff86698
phphe <phphe@outlook.com>
2017-08-12 12:34:24 +0800

    Show Data Thread

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,40 +2,58 @@
   <v-layout row wrap>
     <v-flex xs4>
       <v-select
-       v-bind:items="items"
-       v-model="e1"
-       label="Select"
-       single-line
-       bottom
+       v-bind:items="data1.Objects"
+       item-text="ObjectName"
+       v-model="object"
+       label="Object"
        ></v-select>
     </v-flex>
     <v-flex xs4>
       <v-select
-       v-bind:items="items"
-       v-model="e1"
-       label="Select"
-       single-line
-       bottom
+      :disabled="!object"
+       v-bind:items="object ? object.Items : []"
+       item-text="plal"
+       v-model="item"
+       label="Item"
        ></v-select>
+    </v-flex>
+    <v-flex xs4 style="align-items:center;">
+      {{data1.DateTime}}
     </v-flex>
     <v-flex xs12>
       <v-data-table
-        v-bind:headers="table.headers"
-        :items="table.items"
-        hide-actions
-        class="elevation-1"
-      >
-      <template slot="items" scope="props">
-        <td>{{ props.item.name }}</td>
-        <td class="text-xs-right">{{ props.item.calories }}</td>
-        <td class="text-xs-right">{{ props.item.fat }}</td>
-        <td class="text-xs-right">{{ props.item.carbs }}</td>
-        <td class="text-xs-right">{{ props.item.protein }}</td>
-        <td class="text-xs-right">{{ props.item.sodium }}</td>
-        <td class="text-xs-right">{{ props.item.calcium }}</td>
-        <td class="text-xs-right">{{ props.item.iron }}</td>
-      </template>
-    </v-data-table>
+          :items="itemRows1"
+          hide-actions
+          class="elevation-1"
+        >
+        <template slot="items" scope="props">
+          <td class="">{{ props.item.name }}</td>
+          <td class="text-xs-right">{{ props.item.value }}</td>
+        </template>
+      </v-data-table>
+    </v-flex>
+    <v-flex xs12 class="mt-3">
+      <div class="">
+        <p>
+            PLCComm: {{data2.Status.PLCComm}}
+            LineMode: {{data2.Status.LineMode}}
+            AlarmStatus: {{data2.Status.AlarmStatus}}
+            NoAlarms: {{data2.Status.NoAlarms}}
+        </p>
+        <v-data-table
+          :headers="headers2"
+          :items="data2.Status.AlarmDetails"
+          hide-actions
+          class="elevation-1"
+        >
+        <template slot="items" scope="props">
+          <td>{{ props.item.Category }}</td>
+          <td>{{ props.item.AlarmStatus }}</td>
+          <td>{{ props.item.AlarmMode }}</td>
+          <td>{{ props.item.Comments }}</td>
+        </template>
+      </v-data-table>
+      </div>
     </v-flex>
   </v-layout>
 </template>
@@ -44,146 +62,128 @@ export default {
   data() {
     return {
       title: 'Home',
-      e1: '',
-      items: [
-        { text: 'State 1' },
-        { text: 'State 2' },
-        { text: 'State 3' },
-        { text: 'State 4' },
-        { text: 'State 5' },
-        { text: 'State 6' },
-        { text: 'State 7' }
-      ],
-      table: {
-        headers: [
+      data1: {
+        'TimeStamp': 1502339657000,
+        'DateTime': '12:34:17 10 Aug 2017',
+        'Objects': [
           {
-            text: 'Dessert (100g serving)',
-            align: 'left',
-            sortable: false,
-            value: 'name'
-          },
-          { text: 'Calories', value: 'calories' },
-          { text: 'Fat (g)', value: 'fat' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Sodium (mg)', value: 'sodium' },
-          { text: 'Calcium (%)', value: 'calcium' },
-          { text: 'Iron (%)', value: 'iron' }
-        ],
-        items: [
-          {
-            value: false,
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            sodium: 87,
-            calcium: '14%',
-            iron: '1%'
-          },
-          {
-            value: false,
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            sodium: 129,
-            calcium: '8%',
-            iron: '1%'
-          },
-          {
-            value: false,
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            sodium: 337,
-            calcium: '6%',
-            iron: '7%'
-          },
-          {
-            value: false,
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            sodium: 413,
-            calcium: '3%',
-            iron: '8%'
-          },
-          {
-            value: false,
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            sodium: 327,
-            calcium: '7%',
-            iron: '16%'
-          },
-          {
-            value: false,
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            sodium: 50,
-            calcium: '0%',
-            iron: '0%'
-          },
-          {
-            value: false,
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            sodium: 38,
-            calcium: '0%',
-            iron: '2%'
-          },
-          {
-            value: false,
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            sodium: 562,
-            calcium: '0%',
-            iron: '45%'
-          },
-          {
-            value: false,
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            sodium: 326,
-            calcium: '2%',
-            iron: '22%'
-          },
-          {
-            value: false,
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            sodium: 54,
-            calcium: '12%',
-            iron: '6%'
+            'ObjectName': 'temp',
+            'Items': [
+              {
+                'plal': 20,
+                'pllm': 40,
+                'tact': 0,
+                'tout': 0,
+                'phlm': 60,
+                'phal': 80
+              },
+              {
+                'plal': 20,
+                'pllm': 40,
+                'tact': 0,
+                'tout': 0,
+                'phlm': 60,
+                'phal': 80
+              },
+              {
+                'plal': 20,
+                'pllm': 40,
+                'tact': 0,
+                'tout': 0,
+                'phlm': 60,
+                'phal': 80
+              },
+              {
+                'plal': 20,
+                'pllm': 40,
+                'tact': 0,
+                'tout': 0,
+                'phlm': 60,
+                'phal': 80
+              },
+              {
+                'plal': 20,
+                'pllm': 40,
+                'tact': 0,
+                'tout': 0,
+                'phlm': 60,
+                'phal': 80
+              },
+              {
+                'plal': 20,
+                'pllm': 40,
+                'tact': 0,
+                'tout': 0,
+                'phlm': 60,
+                'phal': 80
+              }
+            ]
           }
         ]
-      }
+      },
+      object: null,
+      item: null,
+      data2: {
+        'TimeStamp': 1502339232000,
+        'DateTime': '12:27:12 10 Aug 2017',
+        'Status': {
+          'PLCComm': 'UP',
+          'LineMode': 'AUTO',
+          'AlarmStatus': 'UNACKALARMS',
+          'NoAlarms': 6,
+          'AlarmDetails': [
+            {
+              'Category': 'alarm',
+              'AlarmStatus': 'ON',
+              'AlarmMode': 'UNACKALARM',
+              'Comments': 'Temperature 1 Alarm'
+            },
+            {
+              'Category': 'alarm',
+              'AlarmStatus': 'ON',
+              'AlarmMode': 'UNACKALARM',
+              'Comments': 'Temperature 2 Alarm'
+            },
+            {
+              'Category': 'alarm',
+              'AlarmStatus': 'ON',
+              'AlarmMode': 'UNACKALARM',
+              'Comments': 'Temperature 3 Alarm'
+            },
+            {
+              'Category': 'alarm',
+              'AlarmStatus': 'ON',
+              'AlarmMode': 'UNACKALARM',
+              'Comments': 'Temperature 4 Alarm'
+            },
+            {
+              'Category': 'alarm',
+              'AlarmStatus': 'ON',
+              'AlarmMode': 'UNACKALARM',
+              'Comments': 'Temperature 5 Alarm'
+            },
+            {
+              'Category': 'alarm',
+              'AlarmStatus': 'ON',
+              'AlarmMode': 'UNACKALARM',
+              'Comments': 'Temperature 6 Alarm'
+            }
+          ]
+        }
+      },
     }
+  },
+  computed: {
+    itemRows1() {
+      return Object.keys(this.item || {}).map(key => {
+        return { name: key, value: this.item[key] }
+      })
+    },
+    headers2() {
+      return Object.keys(this.data2.Status.AlarmDetails[0]).map(key => {
+        return { text: key, value: key }
+      })
+    },
   },
   mounted() {
     this.$nextTick(() => {
